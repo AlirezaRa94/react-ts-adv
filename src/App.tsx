d270b03b024d0740fb2d6ddd6e8f14108a26387f
Alireza Raei <alirezaraei17@gmail.com>
2024-12-12 20:58:54 +1100

    feat: Add forwardRef to Input component and useRef in App
    - Updated Input component to use forwardRef for forwarding refs
    - Modified App component to use useRef with Input component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
+import { useEffect, useRef } from "react";
 import Button from "./components/Button";
 import Container from "./components/Container";
 import Input from "./components/Input";
 
 function App() {
+  const inputEl = useRef<HTMLInputElement>(null);
+
   return (
     <main>
-      <Input id='name' label='Your Name' type='text' />
+      <Input id='name' label='Your Name' type='text' ref={inputEl} />
       <Input id='age' label='Your Age' type='number' />
       <p>
         <Button el='button'>Submit</Button>
