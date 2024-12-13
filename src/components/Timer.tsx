46f2984342bb217f1331211336d9f2fd8bccb29a
Alireza Raei <alirezaraei17@gmail.com>
2024-12-13 14:57:22 +1100

    feat: Display timers using TimersContext in Timers component
    - Updated Timer component to accept name and duration props
    - Updated Timers component to render a list of Timer components from TimersContext
    - Exported Timer type from TimersContext for reuse

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -1,9 +1,11 @@
-import Container from './UI/Container.tsx';
+import Container from "./UI/Container.tsx";
+import { Timer as TimerProps } from "../contexts/TimersContext.tsx";
 
-export default function Timer() {
+export default function Timer({ name, duration }: TimerProps) {
   return (
-    <Container as="article">
-      <h2>TODO: TIMER NAME</h2>
+    <Container as='article'>
+      <h2>{name}</h2>
+      <p>{duration}</p>
     </Container>
   );
 }
