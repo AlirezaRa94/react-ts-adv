2647e8d9eb61813ac481c364296eed7ed20a5457
Alireza Raei <alirezaraei17@gmail.com>
2024-12-13 19:10:49 +1100

    feat: Add countdown functionality to Timer component
    - Added state and effect to manage countdown timer
    - Displayed remaining duration with a progress bar and formatted text
    - Integrated timer with TimersContext to start/stop based on context state

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -1,11 +1,43 @@
 import Container from "./UI/Container.tsx";
-import { Timer as TimerProps } from "../contexts/TimersContext.tsx";
+import {
+  Timer as TimerProps,
+  useTimersContext,
+} from "../contexts/TimersContext.tsx";
+import { useEffect, useRef, useState } from "react";
 
 export default function Timer({ name, duration }: TimerProps) {
+  const [durationLeft, setDurationLeft] = useState<number>(duration * 1000);
+  const { isRunning } = useTimersContext();
+  const interval = useRef<number | null>(null);
+
+  const hasFinished = durationLeft <= 0 || !isRunning;
+
+  useEffect(() => {
+    if (hasFinished) {
+      interval.current && clearInterval(interval.current);
+      return;
+    }
+
+    interval.current = setInterval(() => {
+      setDurationLeft((prevDuration) => prevDuration - 50);
+    }, 50);
+
+    return () => {
+      interval.current && clearInterval(interval.current);
+    };
+  }, [hasFinished]);
+
+  const formattedDurationLeft = (durationLeft / 1000).toFixed(2);
+
   return (
     <Container as='article'>
       <h2>{name}</h2>
-      <p>{duration}</p>
+      <p>
+        <progress value={durationLeft} max={duration * 1000}>
+          {durationLeft}
+        </progress>
+      </p>
+      <p>{formattedDurationLeft}s</p>
     </Container>
   );
 }
